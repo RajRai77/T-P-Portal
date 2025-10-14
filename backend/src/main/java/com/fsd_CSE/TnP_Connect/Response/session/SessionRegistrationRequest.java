34a038373daadcfd1172bf878b95839d4aa31534
Raj Rai <rajrai77@gmail.com>
2025-10-14 10:10:00 +0530

    add session DTOs

---
+++ backend/src/main/java/com/fsd_CSE/TnP_Connect/Response/session/SessionRegistrationRequest.java
@@ -0,0 +1,12 @@
+package com.fsd_CSE.TnP_Connect.Response.session;
+
+public class SessionRegistrationRequest {
+    private Integer studentId;
+    private Integer sessionId;
+
+    public Integer getStudentId() { return studentId; }
+    public void setStudentId(Integer studentId) { this.studentId = studentId; }
+    public Integer getSessionId() { return sessionId; }
+    public void setSessionId(Integer sessionId) { this.sessionId = sessionId; }
+
+}
