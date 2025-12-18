4efa4adb0fae8f415a15af0f10261c68613076f7
Raj Rai <rajrai77@gmail.com>
2025-12-18 10:00:00 +0530

    add auth module and routing

---
+++ frontend2/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PendingApprovalComponent } from './components/pending-approval/pending-approval.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'pending-approval', component: PendingApprovalComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
