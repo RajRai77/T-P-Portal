31d2c9084ae6c3b4b2e5ac9c90ad1957a12380c2
Raj Rai <rajrai77@gmail.com>
2025-12-24 10:00:00 +0530

    add PendingApprovalComponent

---
+++ frontend2/src/app/auth/components/pending-approval/pending-approval.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-approval',
+  standalone: false,
+  templateUrl: './pending-approval.component.html',
+  styleUrl: './pending-approval.component.css'
+})
+export class PendingApprovalComponent {
+
+}
